90afab327e8c3f8e063782da6e75ddf2866bbf26
Stergios Skolarikos <smskolarikos@gmail.com>
2021-10-21 12:29:43 +0300

    Refactor RabbitMQBus for Dependency Injection for parameterless constructors in our event handlers

---
+++ MicroRabbit/MicroRabbit.Transfer.Api/Controllers/TransferController.cs
@@ -0,0 +1,35 @@
+﻿using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Transfer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransferController : ControllerBase
+    {
+        private readonly ITransferService _transferService;
+        public TransferController (ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+        //GET api/banking
+        [HttpGet]
+        public ActionResult<IEnumerable<TransferLog>> Get()
+        {
+            
+            return Ok(_transferService.GetTransferLogs());
+        }
+        //[HttpPost]
+        //public IActionResult Post([FromBody] AccountTransfer accountTransfer)
+        //{
+        //    _accountService.Transfer(accountTransfer);
+        //    return Ok(accountTransfer);
+        //}
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Transfer.Data/Migrations/20211019120749_Initial Migration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace MicroRabbit.Transfer.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TransferLogs",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FromAccount = table.Column<int>(type: "integer", nullable: false),
+                    ToAccount = table.Column<int>(type: "integer", nullable: false),
+                    TransferAmount = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransferLogs", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TransferLogs");
+        }
+    }
+}
